95fb672cb7973ce241cbc9dce19f9dde3a25303b
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 20:20:04 +0800

    project done

--- extract.py
+++ extract.py
@@ -3,6 +3,13 @@
 # Pseudocode
 
 # convert integer to string then reverse
+def extract_reverse(number):
+    reversed = str(number)[::-1]
 # extract digit
+    for digit_str in reversed:
+        number = int(digit_str)
 # print
+        print(number)  
 # user input code
+user_input = int(input("please provide a number: "))
+extract_reverse(user_input)
