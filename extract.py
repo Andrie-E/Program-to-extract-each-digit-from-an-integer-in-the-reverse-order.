28f4b8c37ea7302c07418e63c3c21cacb8f4fc4c
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 20:12:38 +0800

    pseudocode

--- extract.py
+++ extract.py
@@ -1,1 +1,8 @@
 # Write a Program to extract each digit from an integer in the reverse order.
+
+# Pseudocode
+
+# convert integer to string then reverse
+# extract digit
+# print
+# user input code
